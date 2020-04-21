0486454a39732b694572bf559c2d6dcf5efffed6
YashJipkate <yashjipkate@gmail.com>
2020-04-22 01:04:54 +0530

    Revert "Added private repo submodule"
    This reverts commit 0c41460fd9e19091a62bfeaa81b39dd089134f1e.

--- pull_personal_files.sh
+++
@@ -1,5 +0,0 @@
-# This file copies the contents of the confidential-content-personal-site repository
-# to the respective folders in the repo.
-
-cp -a confidential-content-personal-site/images/. public/images/personal/
-cp -a confidential-content-personal-site/pages/. pages/personal/
