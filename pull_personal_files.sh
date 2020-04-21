0c41460fd9e19091a62bfeaa81b39dd089134f1e
YashJipkate <yashjipkate@gmail.com>
2020-04-22 00:40:45 +0530

    Added private repo submodule

---
+++ pull_personal_files.sh
@@ -0,0 +1,5 @@
+# This file copies the contents of the confidential-content-personal-site repository
+# to the respective folders in the repo.
+
+cp -a confidential-content-personal-site/images/. public/images/personal/
+cp -a confidential-content-personal-site/pages/. pages/personal/
