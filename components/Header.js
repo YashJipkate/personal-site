57188b98f569cf1cc495606507bc1abe2fbb1dc1
YashJipkate <yashjipkate@gmail.com>
2021-01-16 12:05:51 +0530

    Added support for markdown (fix #5) and set up blogs (fix #4)

--- components/Header.js
+++ components/Header.js
@@ -1,91 +1,85 @@
 import React from "react";
 
+import Head from "next/head";
+import Link from "next/link";
+
 export default class Header extends React.Component {
   render() {
     return (
-      <nav
-        className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light"
-        id="ftco-navbar"
-      >
-        <div className="container">
-          <button
-            className="navbar-brand btn"
-            onClick={() => document.getElementById("home").scrollIntoView()}
-          >
-            YJ
-          </button>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#ftco-nav"
-            aria-controls="ftco-nav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="oi oi-menu"></span> Menu
-          </button>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Barlow+Condensed:900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap"
+          rel="stylesheet"
+        />
+        <link href="/css/icomoon.css" rel="stylesheet" />
+        <link rel="shortcut icon" href="/favicon.png" key="icon" />
+        <title key="title">{this.props.title} | Yash Jipkate</title>
+        <meta name="author" content="Yash Jipkate" />
+        <meta
+          name="description"
+          content="This is the official portfolio website of Yash Jipkate. It contains all the information and contact details that aims to give the visitor complete look into the professional life of Yash Jipkate."
+        />
+        <meta
+          property="og:image"
+          content="https://cdn.dribbble.com/users/2424687/screenshots/6065697/cat-01.png"
+        />
+        <meta
+          property="og:description"
+          content="This is the official portfolio website of Yash Jipkate. It contains all the information and contact details that aims to give the visitor complete look into the professional life of Yash Jipkate."
+        />
+        <meta property="og:title" content={`${this.props.title} | Yash Jipkate`} />
 
-          <div className="collapse navbar-collapse" id="ftco-nav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <span className="nav-link">
-                  <u>Jr. Undergraduate, IIT(BHU)</u>
-                </span>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="/files/resume.pdf"
-                  target="_blank"
-                >
-                  Resume
-                </a>
-              </li>
-              <li className="nav-item">
-                <button
-                  className="nav-link btn"
-                  onClick={() =>
-                    document.getElementById("about").scrollIntoView()
-                  }
-                >
-                  About
-                </button>
-              </li>
-              <li className="nav-item">
-                <button
-                  className="nav-link btn"
-                  onClick={() =>
-                    document.getElementById("technical").scrollIntoView()
-                  }
-                >
-                  Technical
-                </button>
-              </li>
-              <li className="nav-item">
-                <button
-                  className="nav-link btn"
-                  onClick={() =>
-                    document.getElementById("positions").scrollIntoView()
-                  }
-                >
-                  Positions
-                </button>
-              </li>
-              <li className="nav-item">
-                <button
-                  className="nav-link btn"
-                  onClick={() =>
-                    document.getElementById("contact").scrollIntoView()
-                  }
-                >
-                  Contact
-                </button>
-              </li>
-            </ul>
+        <nav
+          className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light"
+          id="ftco-navbar"
+        >
+          <div className="container">
+            <Link href="/">
+              <a className="navbar-brand btn">
+                YJ
+              </a>
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#ftco-nav"
+              aria-controls="ftco-nav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="oi oi-menu"></span> Menu
+            </button>
+
+            <div className="collapse navbar-collapse" id="ftco-nav">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <span className="nav-link">
+                    <u>Jr. Undergraduate, IIT(BHU)</u>
+                  </span>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link"
+                    href="/files/resume.pdf"
+                    target="_blank"
+                  >
+                    Resume
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </Head>
     );
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-import Head from "next/head";
-
+import BlogSection from "../components/BlogSection";
 import Header from "../components/Header";
 import Landing from "../components/Landing";
 import About from "../components/About";
@@ -23,56 +22,13 @@ export default class Index extends React.Component {
   render() {
     return (
       <div>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css?family=Barlow+Condensed:900&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap"
-            rel="stylesheet"
-          />
-          <link href="/css/icomoon.css" rel="stylesheet" />
-          <link rel="shortcut icon" href="/favicon.png" key="icon" />
-          <title key="title">Yash Jipkate | Personal</title>
-          <meta name="author" content="Yash Jipkate" />
-          <meta
-            name="description"
-            content="This is the official portfolio website of Yash Jipkate. It contains all the information and contact details that aims to give the visitor complete look into the professional life of Yash Jipkate."
-          />
-          <meta
-            property="og:image"
-            content="https://cdn.dribbble.com/users/2424687/screenshots/6065697/cat-01.png"
-          />
-          <meta
-            property="og:description"
-            content="This is the official portfolio website of Yash Jipkate. It contains all the information and contact details that aims to give the visitor complete look into the professional life of Yash Jipkate."
-          />
-          <meta property="og:title" content="Yash Jipkate | Personal" />
-        </Head>
-        <Header />
+        <Header title="Home" />
         <Landing />
         <About />
+        <BlogSection />
         <TechnicalExperiences />
         <ResponsibilityPositions />
         <Footer />
-        <script src="/js/jquery.min.js"></script>
-        <script src="/js/jquery-migrate-3.0.1.min.js"></script>
-        <script src="/js/popper.min.js"></script>
-        <script src="/js/bootstrap.min.js"></script>
-        <script src="/js/jquery.easing.1.3.js"></script>
-        <script src="/js/jquery.waypoints.min.js"></script>
-        <script src="/js/jquery.stellar.min.js"></script>
-        <script src="/js/owl.carousel.min.js"></script>
-        <script src="/js/jquery.magnific-popup.min.js"></script>
-        <script src="/js/aos.js"></script>
-        <script src="/js/jquery.animateNumber.min.js"></script>
-        <script src="/js/scrollax.min.js"></script>
-        <script src="/js/main.js"></script>
       </div>
     );
   }
